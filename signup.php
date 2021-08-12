cbf9858bd9b1816a25072733aaa461e84d4edfca
suryatap <85733808+suryatap@users.noreply.github.com>
2021-08-12 20:41:31 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,27 +6,28 @@ $lname = mysqli_real_escape_string($conn, $_POST['lname']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
 if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){//if email id is valid
         $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-        if(mysqli_num_rows($sql) > 0){
+        if(mysqli_num_rows($sql) > 0){//if email id exists
             echo "$email - This email already exist!";
         }else{
-            if(isset($_FILES['image'])){
-                $img_name = $_FILES['image']['name'];
-                $img_type = $_FILES['image']['type'];
-                $tmp_name = $_FILES['image']['tmp_name'];
+            //checking user uploads file or not
+            if(isset($_FILES['image'])){//if file is uploaded
+                $img_name = $_FILES['image']['name'];//getting users image name
+                $img_type = $_FILES['image']['type'];//getting users imagetype
+                $tmp_name = $_FILES['image']['tmp_name'];//save or move file
                 
                 $img_explode = explode('.',$img_name);
                 $img_ext = end($img_explode);
 
                 $extensions = ["jpeg", "png", "jpg"];
-                if(in_array($img_ext, $extensions) === true){
+                if(in_array($img_ext, $extensions) === true){//if user uploaded image extension is matched with any array extension
                     $types = ["image/jpeg", "image/jpg", "image/png"];
                     if(in_array($img_type, $types) === true){
-                        $time = time();
+                        $time = time();//return current time
                         $new_img_name = $time.$img_name;
                         if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
-                            $ran_id = rand(time(), 100000000);
+                            $ran_id = rand(time(), 100000000);//random id for user
                             $status = "Active now";
                             $encrypt_pass = md5($password);
                             $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
