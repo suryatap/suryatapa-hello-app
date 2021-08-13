6cc998be7a85767748a9c96227e5c36545b0fd9a
suryatap <85733808+suryatap@users.noreply.github.com>
2021-08-13 22:46:19 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,55 @@
+const searchBar = document.querySelector(".search input"),
+searchBtn = document.querySelector(".search button"),
+usersList = document.querySelector(".users-list");
+
+  searchBtn.onclick = ()=>{
+  searchBar.classList.toggle("active");
+  
+  searchBar.focus();
+  
+
+    searchBtn.classList.toggle("active");
+    searchBar.value = "";
+}
+
+
+
+searchBar.onkeyup = ()=>{
+  let searchTerm = searchBar.value;
+ if(searchTerm !=  ""){
+  searchBar.classList.add("active");
+}else{
+  searchBar.classList.remove("active");
+}
+ 
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/searc.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          usersList.innerHTML = data;
+        }
+    }
+  }
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("searchTerm=" + searchTerm);
+}
+
+setInterval(() =>{
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "php/users.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          if(!searchBar.classList.contains("active")){
+            usersList.innerHTML = data;
+          }
+
+          
+        }
+    }
+  }
+  xhr.send();
+}, 500);
